b2908275383efed0cb1efab334593c25459b60d0
Raymond <96218923+raymond-le-goldenowl@users.noreply.github.com>
2022-04-02 07:39:44 +0700

    feat: build component main section (#4)
    * chore: add sass
    
    * chore: create files setup for LinkButton component
    
    * config fonts
    
    * chore: add classnames package
    
    * chore(package.json): change app name
    
    * feat: add component link button
    
    * fix: clean app.js
    
    * feat: create menu
    
    * feat: add banner header
    
    * fix: clean App.js
    
    * fix: change props for LinkButton component
    
    * fix: change color and background for vertical button on header
    
    * fix: background layer for header
    
    * chore(header): update width of header
    
    * chore(header): change logo from text to image
    
    * fix: update header layer background with responsive
    
    * fix: change styles for sign up button
    
    * fix: transition nav links
    
    * feat: create useWindowSize
    
    * fix: change arrow dark to arrow light if end of tablet breakpoint
    
    * fix: update styles for submenu
    
    * chore: change padding top of header nav
    
    * fix: update position of mobile nav links
    
    * fix: image arrow light and arrow dark
    
    * add new mixins
    
    * feat: styles footer component
    
    * chore: merge header and footer
    
    * fix: update border radius top right
    
    * chore: setup content
    
    * fix: fix styles for middle section
    
    * fix: change name page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from 'components/Header';
+import Footer from 'components/Footer';
+import MainSection from 'components/MainSection';
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
+			<MainSection />
+			<Footer />
 		</div>
 	);
 }
